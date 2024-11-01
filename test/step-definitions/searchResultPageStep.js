6e1cf7da1edc8c89ce7584a3994bb0eb812afc80
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-11-01 11:56:44 +0600

    new step added

--- test/step-definitions/searchResultPageStep.js
+++ test/step-definitions/searchResultPageStep.js
@@ -2,6 +2,10 @@ import { Then } from "@wdio/cucumber-framework";
 import { assert } from "chai";
 import SearchResultPage from "../page-objects/SearchResultPage.js";
 
-Then(/^I should see (.*) in the search results$/, async (searchValue) => {
-  assert.include(await SearchResultPage.getSearchResult(), searchValue);
+Then(/^I should see "(.*)" in the search results$/, async (searchValue) => {
+  assert.include(await SearchResultPage.getSearchResult(), searchValue, "search value not showing in search result page");
+});
+
+Then(/^"(.*)" should be displayed in the search results$/, async (message) => {
+  assert.strictEqual(await SearchResultPage.getNoSearchProductText(), message, `${message} is not displaying in the search result page`);
 });
