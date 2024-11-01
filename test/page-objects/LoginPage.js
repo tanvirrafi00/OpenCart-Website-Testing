a9bc4cecc35ff8843eb8c7b0c57a3a8dcd18a061
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-11-01 11:55:31 +0600

    login page elements added

---
+++ test/page-objects/LoginPage.js
@@ -0,0 +1,23 @@
+import { Button, Input, Label } from "../../framework/elements";
+import BasePage from "../../framework/page/BasePage.js";
+import { PreciseTextLocator } from "../../framework/utils/locatorHelper.js";
+
+class LoginPage extends BasePage {
+  constructor() {
+    super(new Label(PreciseTextLocator(`Returning Customer`)), "Login page");
+    this.emailField = new Input(`//input[@name="email"]`, "Email Field");
+    this.passwordField = new Input(`//input[@name="password"]`, "Password Field");
+    this.loginButton = new Button(`//button[@class="btn btn-primary"]`, "Login Button");
+  }
+
+  async Login(email, password) {
+    await this.emailField.typeText(email);
+    await this.passwordField.typeText(password);
+  }
+
+  async clickLoginButton() {
+    await this.loginButton.click();
+  }
+}
+
+export default new LoginPage();
