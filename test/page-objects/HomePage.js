bc3a41d955ce4898b45a5ae3eb2d2f43deb00245
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-11-01 11:55:14 +0600

    new element added

--- test/page-objects/HomePage.js
+++ test/page-objects/HomePage.js
@@ -6,6 +6,8 @@ class HomePage extends BasePage {
 
     this.serachField = new Input(`//input[@name="search"]`, "Search field");
     this.searchButton = new Button(`//button[@class="btn btn-light btn-lg"]`, "Search Button");
+    this.myAccountOption = new Label(`//div[@class="nav float-end"]//li[@class="list-inline-item"][2]`, "My account option");
+    this.loginLink = new Label(`//ul[@class="dropdown-menu dropdown-menu-right show"]//li[2]`, "Login Link");
   }
 
   async setValueInSearchField(value) {
@@ -14,6 +16,15 @@ class HomePage extends BasePage {
 
   async clickSearchButton() {
     await this.searchButton.click();
+  }
+
+  async clickMyAccount() {
+    await this.myAccountOption.click();
+  }
+
+  async clickLogin() {
+    await this.loginLink.state().waitForDisplayed();
+    await this.loginLink.click();
   }
 }
 
