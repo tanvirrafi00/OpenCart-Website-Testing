171d54b0bb161c5d167a3c0d14884e9a052deba0
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-11-01 12:38:56 +0600

    browser stack service added

--- configs/chrome.cucumber.conf.js
+++ configs/chrome.cucumber.conf.js
@@ -20,7 +20,44 @@ export const config = {
       ],
     ],
     specs: ["../test/features/**/*.feature"],
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
+
+    services: [
+      [
+        "browserstack",
+        {
+          testObservability: true,
+
+          browserstackLocal: true,
+        },
+      ],
+    ],
     capabilities: [
+      {
+        browserName: "Firefox",
+        "bstack:options": {
+          os: "Windows",
+          osVersion: "11",
+          browserVersion: "130.0",
+        },
+      },
+      {
+        browserName: "Safari",
+        "bstack:options": {
+          os: "OS X",
+          osVersion: "Monterey",
+          browserVersion: "15.6",
+        },
+      },
+      {
+        browserName: "Chromium",
+        "bstack:options": {
+          deviceName: "iPhone 13",
+          osVersion: "15",
+          deviceOrientation: "portrait",
+        },
+      },
       {
         browserName: "chrome",
         "goog:chromeOptions": {
