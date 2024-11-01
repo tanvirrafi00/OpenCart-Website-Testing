573de1e5a843f9cde1ec9117d1280867b1c12f9a
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-11-01 11:55:59 +0600

    new method added

--- test/page-objects/SearchResultPage.js
+++ test/page-objects/SearchResultPage.js
@@ -5,10 +5,15 @@ class SearchResultPage extends BasePage {
   constructor() {
     super(new Label(PreciseTextLocator("Products meeting the search criteria")), "Search Page");
     this.searchResult = new Label(`//h1`, "Search Result");
+    this.noProductText = new Label(`//div[@id="content"]//p`, "no search product text");
   }
 
   async getSearchResult() {
     return await this.searchResult.getText();
+  }
+
+  async getNoSearchProductText() {
+    return await this.noProductText.getText();
   }
 }
 export default new SearchResultPage();
